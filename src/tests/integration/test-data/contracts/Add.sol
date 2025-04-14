ed113e1350409265000d92862dde7110755702c1
Tamás Tóth <tothtamas28@users.noreply.github.com>
2025-04-14 15:30:34 +0200

    Test `ADD` without Solidity ABI  (#8)

--- src/tests/integration/test-data/contracts/Add.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.8.0;
-
-contract Add {
-    function add(uint256 a, uint256 b) public pure returns (uint256 result) {
-        assembly {
-            result := add(a, b)
-        }
-    }
-}
