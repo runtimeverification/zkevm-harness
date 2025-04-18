12667ad167130e4516e9ebfffc4edabc66e9c51a
Stevengre <zhaojianhong96@gmail.com>
2025-04-18 19:18:31 +0800

    initial version

--- src/tests/integration/test_prove.py
+++ src/tests/integration/test_prove.py
@@ -1,21 +1,19 @@
 from __future__ import annotations
 
+from collections.abc import Callable
 from typing import TYPE_CHECKING, Final
 
 import kriscv.term_builder as tb
 import pytest
 from kriscv.elf_parser import _memory_segments, entry_point, read_unique_symbol
-from kriscv.sparse_bytes import SparseBytes, SymBytes
 from pyk.cterm import CSubst, CTerm, cterm_build_claim
 from pyk.kast.inner import KApply, KSequence, KSort, KVariable, Subst
 from pyk.proof.reachability import APRProof, APRProver
+from pyk.proof.show import APRProofShow
 
-from .utils import SP1_CONFIG, TEST_DATA_DIR, _elf_file, build_elf, get_symbols, resolve_symbol
-
-DEBUG_DIR: Final = TEST_DATA_DIR / 'debug'
+from .utils import DEBUG_DIR, SP1_CONFIG, _elf_file, build_elf, get_symbols, resolve_symbol
 
 if TYPE_CHECKING:
-    from collections.abc import Callable
     from pathlib import Path
 
     from kriscv.symtools import SymTools
@@ -35,21 +33,19 @@ def _init_config(
     # 3. set a new halt condition
     # 4. continue the symbolic execution until the new halt condition is reached.
 
-    tmp = {addr: SymBytes(KVariable(var), size) for addr, (size, var) in symdata.items()}
+    data = _memory_segments(elf_file)
+    sparse_bytes = tb.sparse_bytes(data, symdata)
+    (end_symbol,) = get_symbols(elf_file, build_config.end_pattern)
     with _elf_file(elf_file) as elf:
-        data = _memory_segments(elf)
-        sparse_bytes = SparseBytes.from_data(data, tmp)
-        mem, constraints = sparse_bytes.to_k()
-        (end_symbol,) = get_symbols(elf_file, build_config.end_pattern)
         end_addr = read_unique_symbol(elf, end_symbol, error_loc=str(elf_file))
         halt_cond = tb.halt_at_address(tb.word(end_addr))
-        config_vars = {
-            '$REGS': tb.regs(dict.fromkeys(range(32), 0)),
-            '$MEM': mem,
-            '$PC': entry_point(elf),
-            '$HALT': halt_cond,
-        }
-        return CTerm(kriscv.init_config(config_vars), constraints)
+    config_vars = {
+        '$REGS': tb.regs(dict.fromkeys(range(32), 0)),
+        '$MEM': sparse_bytes,
+        '$PC': entry_point(elf_file),
+        '$HALT': halt_cond,
+    }
+    return CTerm(kriscv.init_config(config_vars), tb.sparse_bytes_constraints(symdata))
 
 
 def _final_config(symtools: SymTools) -> CTerm:
@@ -61,7 +57,7 @@ def _final_config(symtools: SymTools) -> CTerm:
 
 
 PROVE_TEST_DATA: Final = (('add-test', 2, SP1_CONFIG),)
-DEPTH: Final = 1000
+DEPTH: Final = 1
 MAX_ITERATIONS: Final = 45
 
 
@@ -71,32 +67,34 @@ MAX_ITERATIONS: Final = 45
     ids=[test_id for test_id, *_ in PROVE_TEST_DATA],
 )
 def test_prove_equivalence(
-    symtools: Callable[[str, str, str], SymTools],
+    symtools: SymTools,
     tools: Callable[[str], Tools],
     load_template: TemplateLoader,
     test_id: str,
     arg_count: int,
     build_config: BuildConfig,
 ) -> None:
-    if test_id in ['add-test']:
-        pytest.skip(f'Skipping {test_id} because we are still working on it')
-
     # Given
-    symtool = symtools(f'{build_config.target}-haskell', f'{build_config.target}-lib', 'zkevm-semantics.source')
     elf_file = build_elf(test_id, load_template, build_config)
-    symdata = {resolve_symbol(elf_file, f'OP{i}'): (32, f'W{i}') for i in range(arg_count)}
+    symdata = {resolve_symbol(elf_file, f'OP{i}'): (32, f'W{i}') for i in range(0, arg_count)}
 
     init_config = _init_config(symdata, build_config, elf_file, tools(build_config.target))
-    kclaim = cterm_build_claim(test_id.upper(), init_config, _final_config(symtool))
-    proof = APRProof.from_claim(symtool.kprove.definition, kclaim[0], {}, symtool.proof_dir)
+    kclaim = cterm_build_claim(test_id.upper(), init_config, _final_config(symtools))
+    proof = APRProof.from_claim(symtools.kprove.definition, kclaim[0], {}, symtools.proof_dir)
 
     # When
-    with symtool.explore(id='ADD') as kcfg_explore:
+    with symtools.explore(id='ADD') as kcfg_explore:
         prover = APRProver(
             kcfg_explore=kcfg_explore,
             execute_depth=DEPTH,
         )
         prover.advance_proof(proof, max_iterations=MAX_ITERATIONS)
+        proof_show = APRProofShow(symtools.kprove)
+        with open(DEBUG_DIR / 'proof-result.txt', 'w') as f:
+            f.write('\n'.join(proof_show.show(proof, [node.id for node in proof.kcfg.nodes])))
 
-    # Then: Prove `R(S_{REVM}.initial, S_{KEVM}.initial) /\ R(S_{REVM}.final, S_{KEVM}.final)`
-    # `R` is the relation between KEVM state `S_{KEVM}` and REVM State in RISC-V memory `S_{REVM}`
+    # Then
+    # Given the `REVM` in the riscv memory as `S_{REVM}`, and the `KEVM` state as `S_{KEVM}`
+    # Define a relation `R(S_{REVM}, S_{KEVM})`
+    # Prove that `R(S_{REVM}, S_{KEVM})` is valid for their initial state and final state.
+    # That is `R(S_{REVM}.initial, S_{KEVM}.initial) /\ R(S_{REVM}.final, S_{KEVM}.final)` refinement verification through forward simulation.
