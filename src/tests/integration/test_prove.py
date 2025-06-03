fe085df8d3094171b0ce8735c832372fe561cd5f
Tamás Tóth <tothtamas28@users.noreply.github.com>
2025-06-03 11:04:01 +0200

    Add test case for K claim generation (#25)

--- src/tests/integration/test_prove.py
+++ src/tests/integration/test_prove.py
@@ -3,65 +3,108 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Final
 
 import pytest
-from pyk.cterm import CTerm, cterm_build_claim
-from pyk.kast.inner import KApply, KSequence, KSort, KVariable, Subst
-from pyk.kast.manip import free_vars
-from pyk.proof.reachability import APRProof, APRProver
 
-from .utils import SP1_CONFIG, build_elf, filter_symbols
+from .utils import SP1_CONFIG, SPEC_DIR
 
 if TYPE_CHECKING:
     from collections.abc import Callable
-    from pathlib import Path
 
-    from kriscv.elf_parser import ELF
     from kriscv.symtools import SymTools
     from kriscv.tools import Tools
 
     from .utils import BuildConfig, TemplateLoader
 
 
-PROVE_TEST_DATA: Final = (('add-test', 2, SP1_CONFIG),)
-DEPTH: Final = 1000
+MAX_DEPTH: Final = 1000
 MAX_ITERATIONS: Final = 45
 
 
+GEN_TEST_DATA: Final = (('add-test-sp1', SP1_CONFIG, 'add-test', ['OP0', 'OP1']),)
+PROVE_TEST_DATA: Final = tuple((test_id, build_config) for test_id, build_config, *_ in GEN_TEST_DATA)
+
+
+@pytest.mark.skip
 @pytest.mark.parametrize(
-    'test_id,arg_count,build_config',
+    'test_id,build_config,project_name,symbolic_names',
+    GEN_TEST_DATA,
+    ids=[test_id for test_id, *_ in GEN_TEST_DATA],
+)
+def test_generate_claim(
+    tools: Callable[[str], Tools],
+    symtools: Callable[[str, str, str], SymTools],
+    load_template: TemplateLoader,
+    # ---
+    test_id: str,
+    build_config: BuildConfig,
+    project_name: str,
+    symbolic_names: list[str],
+) -> None:
+    from pyk.ktool.claim_loader import ClaimLoader
+
+    from zkevm_harness.utils import halt_claim_from_elf, spec_module_text
+
+    from .utils import build_elf, filter_symbols
+
+    # Given
+    tool = tools(build_config.target)
+    spec_file = SPEC_DIR / f'{test_id}.k'
+
+    elf = build_elf(project_name, load_template, build_config)
+    (end_symbol,) = filter_symbols(elf, build_config.end_pattern)
+    claim = halt_claim_from_elf(
+        tools=tool,
+        elf=elf,
+        label=test_id,
+        end_symbol=end_symbol,
+        symbolic_names=symbolic_names,
+    )
+    module_text = spec_module_text(
+        tools=tool,
+        module_name=test_id.upper(),
+        claim=claim,
+    ).strip()
+
+    # When
+    spec_file.write_text(module_text)
+
+    # Then
+    symtool = symtools(f'{build_config.target}-haskell', f'{build_config.target}-lib', 'zkevm-semantics.source')
+    claim_label = f'{test_id.upper()}.{test_id}'
+    assert ClaimLoader(symtool.kprove).load_claims(
+        spec_file=spec_file,
+        claim_labels=[claim_label],
+    )
+
+
+@pytest.mark.parametrize(
+    'test_id,build_config',
     PROVE_TEST_DATA,
     ids=[test_id for test_id, *_ in PROVE_TEST_DATA],
 )
 def test_prove_equivalence(
-    custom_temp_dir: Path,
     symtools: Callable[[str, str, str], SymTools],
-    tools: Callable[[str], Tools],
-    load_template: TemplateLoader,
+    # ---
     test_id: str,
-    arg_count: int,
     build_config: BuildConfig,
 ) -> None:
-    if test_id in ['add-test']:
-        pytest.skip(f'Skipping {test_id} because we are still working on it')
+    if test_id in ['add-test-sp1']:
+        pytest.skip('Work in progress')
 
     # Given
-    tool = tools(build_config.target)
     symtool = symtools(f'{build_config.target}-haskell', f'{build_config.target}-lib', 'zkevm-semantics.source')
-    if APRProof.proof_data_exists(test_id.upper(), symtool.proof_dir):
-        proof = APRProof.read_proof_data(proof_dir=symtool.proof_dir, id=test_id.upper())
-    else:
-        elf = build_elf(test_id, load_template, build_config)
-        init_config = _init_config(tool, elf=elf, build_config=build_config, arg_count=arg_count)
-        final_config = _final_config(symtool)
-        kclaim = cterm_build_claim(test_id.upper(), init_config, final_config)
-        proof = APRProof.from_claim(symtool.kprove.definition, kclaim[0], {}, symtool.proof_dir)
+
+    spec_file = SPEC_DIR / f'{test_id}.k'
+    spec_module_name = test_id.upper()
+    claim_id = f'{spec_module_name}.{test_id}'
 
     # When
-    with symtool.explore(id=test_id.upper()) as kcfg_explore:
-        prover = APRProver(
-            kcfg_explore=kcfg_explore,
-            execute_depth=DEPTH,
-        )
-        prover.advance_proof(proof, max_iterations=MAX_ITERATIONS)
+    proof = symtool.prove(
+        spec_file=spec_file,
+        spec_module=spec_module_name,
+        claim_id=claim_id,
+        max_depth=MAX_DEPTH,
+        max_iterations=MAX_ITERATIONS,
+    )
 
     proof_show = symtool.proof_show
     show_result = '\n'.join(proof_show.show(proof, [node.id for node in proof.kcfg.nodes]))
@@ -69,25 +112,3 @@ def test_prove_equivalence(
 
     # Then: Prove `R(S_{REVM}.initial, S_{KEVM}.initial) /\ R(S_{REVM}.final, S_{KEVM}.final)`
     # `R` is the relation between KEVM state `S_{KEVM}` and REVM State in RISC-V memory `S_{REVM}`
-
-
-def _init_config(tools: Tools, *, elf: ELF, build_config: BuildConfig, arg_count: int) -> CTerm:
-    (end_symbol,) = filter_symbols(elf, build_config.end_pattern)
-    init_kast = tools.config_from_elf(
-        elf,
-        regs=dict.fromkeys(range(32), 0),
-        end_symbol=end_symbol,
-        symbolic_names=[f'OP{i}' for i in range(arg_count)],
-    )
-    return CTerm.from_kast(init_kast)
-
-
-def _final_config(symtools: SymTools) -> CTerm:
-    config = symtools.kprove.definition.empty_config(KSort('GeneratedTopCell'))
-    subst = Subst(
-        {
-            'INSTRS_CELL': KSequence(KApply('#HALT'), KApply('#EXECUTE')),
-            **{vname: KVariable(f'FINAL_{vname}') for vname in free_vars(config) if vname != 'INSTRS_CELL'},
-        },
-    )
-    return CTerm(subst(config))
