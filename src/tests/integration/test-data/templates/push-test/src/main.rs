01481081ade29711aec8320a67b18a92fb976a72
Tamás Tóth <tothtamas28@users.noreply.github.com>
2025-06-12 16:44:53 +0200

    Add claims for `PUSH*` opcodes (#30)

---
+++ src/tests/integration/test-data/templates/push-test/src/main.rs
@@ -0,0 +1,97 @@
+{{ src_header }}
+
+use revm_interpreter::interpreter::{Contract, Interpreter, SharedMemory};
+use revm_interpreter::interpreter_action::InterpreterAction;
+use revm_interpreter::opcode::make_instruction_table;
+use revm_interpreter::primitives::specification::CancunSpec;
+use revm_interpreter::primitives::{address, Bytecode, Bytes, U256};
+use revm_interpreter::DummyHost;
+
+#[unsafe(no_mangle)]
+pub static mut OPCODE: u8 = {{ opcode }};
+
+#[unsafe(no_mangle)]
+pub static mut OP0: [u8; {{ arity }}] = {{ value }};
+
+#[unsafe(no_mangle)]
+pub static mut STATE: u8 = 0;
+
+#[unsafe(no_mangle)]
+pub static mut STACK_ADDR: usize = 0;
+
+#[unsafe(no_mangle)]
+pub static mut RESULT: [u8; 32] = [0x00; 32];
+
+fn main() {
+    // Given
+    let input = Bytes::from([]);
+    let bytecode_iter = unsafe {
+        std::iter::once(OPCODE).chain(OP0.iter().copied())
+    };
+    let bytecode = Bytecode::new_raw(Bytes::from_iter(bytecode_iter));
+    let target_address = address!("0x0000000000000000000000000000000000000001");
+    let caller = address!("0x0000000000000000000000000000000000000002");
+    let call_value = U256::ZERO;
+    let contract = Contract::new(
+        input,
+        bytecode,
+        None,
+        target_address,
+        None,
+        caller,
+        call_value,
+    );
+    let gas_limit = 100000;
+    let mut interpreter = Interpreter::new(contract, gas_limit, false);
+
+    unsafe {
+        STACK_ADDR = &interpreter.stack as *const _ as usize;
+    }
+
+    stack_addr_ready();
+    stack_ready();
+
+    let memory = SharedMemory::new();
+    let instruction_table = make_instruction_table::<DummyHost, CancunSpec>();
+    let mut host = DummyHost::default();
+
+    setup_done();
+
+    // When
+    let action = interpreter.run(memory, &instruction_table, &mut host);
+
+    // Then
+    let InterpreterAction::Return { result: _ } = action else {
+        panic!()
+    };
+    let Ok(res) = interpreter.stack.pop() else {
+        panic!()
+    };
+    unsafe {
+        RESULT = res.to_be_bytes();
+    }
+}
+
+#[unsafe(no_mangle)]
+#[inline(never)]
+fn stack_addr_ready() -> () {
+    unsafe {
+        STATE = 1;
+    }
+}
+
+#[unsafe(no_mangle)]
+#[inline(never)]
+fn stack_ready() -> () {
+    unsafe {
+        STATE = 2;
+    }
+}
+
+#[unsafe(no_mangle)]
+#[inline(never)]
+fn setup_done() -> () {
+    unsafe {
+        STATE = 3;
+    }
+}
--- src/tests/integration/test_prove.py
+++ src/tests/integration/test_prove.py
@@ -47,6 +47,16 @@ GEN_TEST_DATA: Final[tuple[tuple[str, BuildConfig, str, dict[str, str], list[str
     ('shr-test-sp1', SP1_CONFIG, 'simple-2-op-test', {'opcode': '0x1c'}, ['OP0', 'OP1']),
     ('sar-test-sp1', SP1_CONFIG, 'simple-2-op-test', {'opcode': '0x1d'}, ['OP0', 'OP1']),
     ('keccak256-test-sp1', SP1_CONFIG, 'simple-2-op-test', {'opcode': '0x20'}, ['OP0', 'OP1']),
+    ('push0-test-sp1', SP1_CONFIG, 'push-test', {'opcode': '0x5f', 'arity': '0', 'value': '[]'}, []),
+    ('push1-test-sp1', SP1_CONFIG, 'push-test', {'opcode': '0x60', 'arity': '1', 'value': '[0x01]'}, ['OP0']),
+    ('push2-test-sp1', SP1_CONFIG, 'push-test', {'opcode': '0x61', 'arity': '2', 'value': '[0x00, 0x01]'}, ['OP0']),
+    (
+        'push3-test-sp1',
+        SP1_CONFIG,
+        'push-test',
+        {'opcode': '0x62', 'arity': '3', 'value': '[0x00, 0x00, 0x01]'},
+        ['OP0'],
+    ),
 )
 PROVE_TEST_DATA: Final = tuple((test_id, build_config) for test_id, build_config, *_ in GEN_TEST_DATA)
 
