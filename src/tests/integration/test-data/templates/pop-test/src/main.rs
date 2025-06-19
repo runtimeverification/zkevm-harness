7d653c83ba1560c149844ffd74cee97dca1d228f
Tamás Tóth <tothtamas28@users.noreply.github.com>
2025-06-19 12:44:20 +0000

    Add `pop-test-sp1`

---
+++ src/tests/integration/test-data/templates/pop-test/src/main.rs
@@ -0,0 +1,57 @@
+{{ src_header }}
+
+use revm_interpreter::interpreter::{Contract, Interpreter, SharedMemory};
+use revm_interpreter::interpreter_action::InterpreterAction;
+use revm_interpreter::opcode::make_instruction_table;
+use revm_interpreter::primitives::specification::CancunSpec;
+use revm_interpreter::primitives::{address, Bytecode, Bytes, U256};
+use revm_interpreter::DummyHost;
+
+const OPCODE: u8 = 0x50;
+
+#[unsafe(no_mangle)]
+pub static mut VALUE: [u8; 32] = [
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xde, 0xad, 0xbe, 0x01,
+];
+
+fn main() {
+    // Given
+    let input = Bytes::from([]);
+    let bytecode = Bytecode::new_raw(Bytes::from([OPCODE]));
+    let target_address = address!("0x0000000000000000000000000000000000000001");
+    let caller = address!("0x0000000000000000000000000000000000000002");
+    let call_value = U256::ZERO;
+    let contract = Contract::new(
+        input,
+        bytecode,
+        None,
+        target_address,
+        None,
+        caller,
+        call_value,
+    );
+    let gas_limit = 100000;
+    let mut interpreter = Interpreter::new(contract, gas_limit, false);
+
+    let value = U256::from_be_bytes(unsafe { VALUE });
+    let Ok(()) = interpreter.stack.push(value) else {
+        panic!()
+    };
+
+    let memory = SharedMemory::new();
+    let instruction_table = make_instruction_table::<DummyHost, CancunSpec>();
+    let mut host = DummyHost::default();
+
+    // When
+    let action = interpreter.run(memory, &instruction_table, &mut host);
+
+    // Then
+    let InterpreterAction::Return { result: _ } = action else {
+        panic!()
+    };
+
+    if interpreter.stack.len() != 0 {
+        panic!();
+    }
+}
