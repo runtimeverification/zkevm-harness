2a66b738e3da7bedfbbc42d9e264b86eca8ccf50
Tamás Tóth <tothtamas28@users.noreply.github.com>
2025-06-24 09:52:20 +0000

    Add `callvalue-test-sp1`

---
+++ src/tests/integration/test-data/templates/callvalue-test/src/main.rs
@@ -0,0 +1,54 @@
+{{ src_header }}
+
+use revm_interpreter::interpreter::{Contract, Interpreter, SharedMemory};
+use revm_interpreter::interpreter_action::InterpreterAction;
+use revm_interpreter::opcode::make_instruction_table;
+use revm_interpreter::primitives::specification::CancunSpec;
+use revm_interpreter::primitives::{address, Bytecode, Bytes, U256};
+use revm_interpreter::DummyHost;
+
+const OPCODE: u8 = 0x34;
+
+#[unsafe(no_mangle)]
+pub static mut VALUE: [u8; 32] = [
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01,
+];
+
+fn main() {
+    // Given
+    let input = Bytes::from([]);
+    let bytecode = Bytecode::new_raw(Bytes::from([OPCODE]));
+    let target_address = address!("0x0000000000000000000000000000000000000001");
+    let caller = address!("0x0000000000000000000000000000000000000002");
+    let call_value = U256::from_be_bytes(unsafe { VALUE });
+    let contract = Contract::new(
+        input,
+        bytecode,
+        None,
+        target_address,
+        None,
+        caller,
+        call_value,
+    );
+    let gas_limit = 100000;
+    let mut interpreter = Interpreter::new(contract, gas_limit, false);
+
+    let memory = SharedMemory::new();
+    let instruction_table = make_instruction_table::<DummyHost, CancunSpec>();
+    let mut host = DummyHost::default();
+
+    // When
+    let action = interpreter.run(memory, &instruction_table, &mut host);
+
+    // Then
+    let InterpreterAction::Return { result: _ } = action else {
+        panic!()
+    };
+    let Ok(actual) = interpreter.stack.pop() else {
+        panic!()
+    };
+    if actual != call_value {
+        panic!()
+    }
+}
