16a96cdfe1f214b9488f068b2f4b69ecb38a33fc
Tamás Tóth <tothtamas28@users.noreply.github.com>
2025-06-25 13:50:36 +0000

    Add `msize-test-sp1`

---
+++ src/tests/integration/test-data/templates/msize-test/src/main.rs
@@ -0,0 +1,56 @@
+{{ src_header }}
+
+use revm_interpreter::interpreter::{Contract, Interpreter, SharedMemory};
+use revm_interpreter::interpreter_action::InterpreterAction;
+use revm_interpreter::opcode::make_instruction_table;
+use revm_interpreter::primitives::specification::CancunSpec;
+use revm_interpreter::primitives::{address, Bytecode, Bytes, U256};
+use revm_interpreter::DummyHost;
+
+const OPCODE: u8 = 0x59;
+
+const MEMORY_SIZE: usize = 256;
+
+#[unsafe(no_mangle)]
+pub static mut SIZE: u8 = 0x02;
+
+
+fn main() {
+    // Given
+    let input = Bytes::from([]);
+    let bytecode = Bytecode::new_raw(Bytes::from([OPCODE]));
+    let target_address = address!("0x0000000000000000000000000000000000000001");
+    let caller = address!("0x0000000000000000000000000000000000000002");
+    let call_value = U256::ZERO;
+    let contract = Contract::new(
+        input,
+        bytecode,
+        None,
+        target_address,
+        None,
+        caller,
+        call_value,
+    );
+    let gas_limit = 100000;
+    let mut interpreter = Interpreter::new(contract, gas_limit, false);
+
+    let mut memory = SharedMemory::with_capacity(MEMORY_SIZE);
+    memory.resize(usize::from(unsafe { SIZE }));
+
+    let instruction_table = make_instruction_table::<DummyHost, CancunSpec>();
+    let mut host = DummyHost::default();
+
+    // When
+    let action = interpreter.run(memory, &instruction_table, &mut host);
+
+    // Then
+    let InterpreterAction::Return { result: _ } = action else {
+        panic!()
+    };
+    let Ok(actual) = interpreter.stack.pop() else {
+        panic!()
+    };
+    if actual != U256::from(unsafe { SIZE }) {
+        panic!()
+    }
+}
