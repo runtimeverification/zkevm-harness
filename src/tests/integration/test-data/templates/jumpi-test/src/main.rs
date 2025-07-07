f315a8f8e287012e3902e132eae406de15003a2f
Tamás Tóth <tothtamas28@users.noreply.github.com>
2025-07-07 13:50:00 +0000

    Improve `jumpi-test`

--- src/tests/integration/test-data/templates/jumpi-test/src/main.rs
+++ src/tests/integration/test-data/templates/jumpi-test/src/main.rs
@@ -1,18 +1,20 @@
 {{ src_header }}
 
-use revm_interpreter::InstructionResult;
 use revm_interpreter::interpreter::{Contract, Interpreter, InterpreterResult, SharedMemory};
 use revm_interpreter::interpreter_action::InterpreterAction;
 use revm_interpreter::opcode::make_instruction_table;
 use revm_interpreter::primitives::specification::CancunSpec;
 use revm_interpreter::primitives::{address, Bytecode, Bytes, U256};
 use revm_interpreter::DummyHost;
+use revm_interpreter::InstructionResult;
 
-const PREFIX_OPCODES: [u8; 2] = [0x57, 0x00]; // JUMPI STOP
-const JUMPDEST_OPCODE: u8 = 0x5b;
+const CODE_PREFIX: [u8; 2] = [0x57, 0x00]; // JUMPI, STOP
+const CODE_SUFFIX: [u8; 2] = [0x5b, 0x00]; // JUMPDEST, STOP
+
+const MAX_CODE_SIZE: usize = 32;
 
 #[unsafe(no_mangle)]
-pub static mut CODE: [u8; 32] = [0x01; 32];
+pub static mut CODE: [u8; MAX_CODE_SIZE] = [0x01; MAX_CODE_SIZE];
 
 #[unsafe(no_mangle)]
 pub static mut CODE_SIZE: usize = 16;
@@ -20,20 +22,20 @@ pub static mut CODE_SIZE: usize = 16;
 #[unsafe(no_mangle)]
 pub static mut COND: [u8; 32] = [0x01; 32];
 
-
 fn main() {
     // Given
 
-    // assume CODE_SIZE <= 32
-    if unsafe { CODE_SIZE } > 32 {
-        return
+    // assume CODE_SIZE <= MAX_CODE_SIZE
+    if unsafe { CODE_SIZE } > MAX_CODE_SIZE {
+        return;
     }
 
-    let input = Bytes::from([]);
+    let input = Bytes::new();
     let bytecode_iter = unsafe {
-        PREFIX_OPCODES.iter()
-        .chain(CODE[..CODE_SIZE].iter())
-        .chain(std::iter::once(&JUMPDEST_OPCODE))
+        CODE_PREFIX
+            .iter()
+            .chain(CODE[..CODE_SIZE].iter())
+            .chain(CODE_SUFFIX.iter())
     };
     let bytecode = Bytecode::new_raw(Bytes::from_iter(bytecode_iter));
     let target_address = address!("0x0000000000000000000000000000000000000001");
@@ -51,34 +53,37 @@ fn main() {
     let gas_limit = 100000;
     let mut interpreter = Interpreter::new(contract, gas_limit, false);
 
-    let Ok(()) = interpreter.stack.push(U256::from_be_bytes(unsafe { COND })) else {
-        panic!()
-    };
-    let Ok(()) = interpreter.stack.push(U256::from(unsafe { CODE_SIZE } + 2)) else {
-        panic!()
-    };
+    let cond = U256::from_be_bytes(unsafe { COND });
+    let counter = U256::from(unsafe { CODE_SIZE } + 2);
+
+    interpreter.stack.push(cond).unwrap();
+    interpreter.stack.push(counter).unwrap();
 
     let memory = SharedMemory::new();
     let instruction_table = make_instruction_table::<DummyHost, CancunSpec>();
     let mut host = DummyHost::default();
+
+    let expected = if cond.is_zero() {
+        2
+    } else {
+        4 + unsafe { CODE_SIZE }
+    };
 
     // When
     let action = interpreter.run(memory, &instruction_table, &mut host);
 
     // Then
     let InterpreterAction::Return {
-        result: InterpreterResult {
-            result: InstructionResult::Stop,
-            output: _,
-            gas: _,
-        }
-    } = action else {
+        result:
+            InterpreterResult {
+                result: InstructionResult::Stop,
+                output: _,
+                gas: _,
+            },
+    } = action
+    else {
         panic!()
     };
-    let pc = interpreter.program_counter();
-    if unsafe { COND } == [0x00; 32] {
-        assert_eq!(pc, 2);
-    } else {
-        assert_eq!(pc, unsafe { CODE_SIZE } + 4);
-    }
+    let actual = interpreter.program_counter();
+    assert_eq!(actual, expected);
 }
